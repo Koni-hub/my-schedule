3aeb841d947de55fa7fc39731253a6d6d791b780
koni-hub <delgadoargie04z@gmail.com>
2026-05-27 22:20:08 +0800

    fix: add missing MONTH_NAMES constant for improved date handling

--- scripts.js
+++ scripts.js
@@ -34,6 +34,21 @@ const DAY_NAMES = [
   "Saturday",
 ];
 
+const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 window.onload = async () => {
   renderTimeline();
   renderWeekly();
